6299c35b0c71bc20dcb6de638311eafe1396260c
kensei14 <yarimizu.kensei14@gmail.com>
2012-08-10 20:51:32 +0900

    change background2

--- js/base.js
+++ js/base.js
@@ -61,57 +61,6 @@ $(document).ready(function() {
 			pos_y_each = pageHeight * i;
 			pages[i].style.top = pos_y_each + "px";
 		}
-
-
-		
-    	/*************************/
-    	/*
-    	var width_header = Number($('#container_fixed').css("width").replace("px", ""));
-    	var pos_header = Math.ceil(pageWidth / 2 -  width_header/ 2);
-    	    	
-    	var pages = document.getElementsByClassName("container_each");
-    	var inner_Height = pageHeight - 300;
-    	var inner_Width = width_header;
-		var pos_y;
-		
-		$("#container_fixed").css({
-			left : pos_header + "px",
-		});
-		$(".container_each").css({
-			width : pageWidth * 0.7 +  "px",
-			height : pageHeight + "px",
-			left : pos_header + "px",
-		});
-		$(".container_each_in").css({
-			width : inner_Width + "px",
-			height : inner_Height + "px"
-		});
-		$(".contents").css({
-			width : inner_Width - 40 + "px",
-			height : inner_Height  - 100 + "px"
-		});
-		var contents = document.getElementsByClassName("contents");
-		for(var i=0; i<contents.length; i++) {
-			//alert(contents[i].childNodes[0]);
-			//console.log(contents[i].childNodes[0]);
-		}
-		
-		var work_imgs = $(".work_image");
-		var work_rslts = $(".work_result");
-		for(var i=0; i<work_imgs.length; i++) {
-			if(work_imgs[i].width == 0) { work_rslts[i].style.width = 550 + "px"; }
-			else { work_rslts[i].style.width = (inner_Width - work_imgs[i].width - 100) + "px"; }
-		}
-		
-		$("#member_list ul").css({
-			width : Math.ceil(inner_Width / 2 - 50) + "px"
-		});
-
-		for(var i=0; i < pages.length; i++) {
-			pos_y = pageHeight * i;
-			pages[i].style.top = pos_y + "px";
-		}
-				*/
     }
 
     function resizeWindow() {
